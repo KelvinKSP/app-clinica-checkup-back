e46d1e62430434fe4a4936aec894c860a52b1579
Thiago <tfbellato@hotmail.com>
2025-08-30 16:29:52 -0300

    build: initial project setup with maven and dependencies

---
+++ appcheckup/src/main/java/br/com/appcheckup/appcheckup/AppcheckupApplication.java
@@ -0,0 +1,13 @@
+package br.com.appcheckup.appcheckup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcheckupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcheckupApplication.class, args);
+	}
+
+}
---
+++ appcheckup/src/test/java/br/com/appcheckup/appcheckup/AppcheckupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appcheckup.appcheckup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcheckupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
