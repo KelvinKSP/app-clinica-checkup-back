a4d3e621fa2f9d688aa1b5d96d37d9fecc7ed728
Thiago <tfbellato@hotmail.com>
2026-03-10 14:40:01 -0300

    chore: limpar repositório removendo arquivos anteriores

--- src/main/java/br/com/appcheckup/repository/PessoaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.appcheckup.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.appcheckup.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
